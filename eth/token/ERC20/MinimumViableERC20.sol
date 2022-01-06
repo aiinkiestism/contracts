5acaed5334088af74c5083226ee4c21927480d79
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2022-01-06 13:22:25 +0900

    Add MinimumViableERC20

---
+++ eth/token/ERC20/MinimumViableERC20.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract MinimumViableERC20 is ERC20, Pausable {
+  constructor(
+    string memory name,
+    string memory symbol,
+    address initialAccount,
+    uint256 initialBalance
+  ) payable ERC20(name, symbol) {
+    _mint(initialAccount, initialBalance);
+  }
+
+  function mint(address account, uint256 amount) public {
+    _mint(account, amount);
+  }
+
+  function burn(address account, uint256 amount) public {
+    _burn(account, amount);
+  }
+
+  function transferInternal(
+    address from,
+    address to,
+    uint256 value
+  ) public {
+    _transfer(from, to, value);
+  }
+
+  function approveInternal(
+    address owner,
+    address spender,
+    uint256 value
+  ) public {
+    _approve(owner, spender, value);
+  }
+
+  /**
+ *
+ * Useful for scenarios such as preventing trades until the end of an evaluation
+ * period, or having an emergency switch for freezing all token transfers in the
+ * event of a large bug.
+ */
+  function _beforeTokenTransfer(
+      address from,
+      address to,
+      uint256 amount
+  ) internal virtual override {
+      super._beforeTokenTransfer(from, to, amount);
+
+      require(!paused(), "ERC20Pausable: token transfer while paused");
+  }
+}
